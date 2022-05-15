fbdfd61f6bf53ded9800f516d8db2255572a0eb0
Ken Howard <ken@kenhoward.dev>
2022-05-14 21:03:38 -0700

    feat: enable auto merge

---
+++ src/auto_merge.ts
@@ -0,0 +1,62 @@
+import type {PullRequestEvent} from '@octokit/webhooks-types'
+import type {graphql} from '@octokit/graphql'
+
+interface AutoMergeResponse {
+  clientMutationId: null | string
+  pullRequest: {
+    id: string
+    state: string
+    autoMergeRequest: {
+      enabledAt: string
+      enableBy: {
+        login: string
+      }
+    }
+  }
+}
+
+interface Client {
+  graphql: typeof graphql
+}
+
+export enum MergeMethod {
+  MERGE = 'MERGE',
+  SQUASH = 'SQUASH',
+  REBASE = 'REBASE'
+}
+
+export async function enableAutoMerge(
+  {pull_request: {node_id: pullRequestId}}: PullRequestEvent,
+  mergeMethod: MergeMethod,
+  client: Client
+): Promise<AutoMergeResponse> {
+  return client.graphql(
+    `
+      mutation(
+        $pullRequestId: ID!,
+        $mergeMethod: PullRequestMergeMethod!
+      ) {
+        enablePullRequestAutoMerge(input: {
+          pullRequestId: $pullRequestId,
+          mergeMethod: $mergeMethod
+        }) {
+          clientMutationId
+          pullRequest {
+            id
+            state
+            autoMergeRequest {
+              enabledAt
+              enabledBy {
+                login
+              }
+            }
+          }
+        }
+      }
+    `,
+    {
+      pullRequestId,
+      mergeMethod
+    }
+  )
+}
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,31 @@
-import * as core from '@actions/core'
+import {MergeMethod, enableAutoMerge} from './auto_merge'
+import {context, getOctokit} from '@actions/github'
+import {debug, getInput, setFailed} from '@actions/core'
+import type {PullRequestEvent} from '@octokit/webhooks-types'
 
 export async function run(): Promise<void> {
   try {
-    const enable = Boolean(core.getInput('enable') === 'true')
-    const token = core.getInput('token')
+    const enable = Boolean(getInput('enable') === 'true')
+    const token = getInput('token')
 
     if (!token) {
       throw new Error("required field 'token' missing or empty")
     }
 
     if (!enable) {
-      core.debug("enable evaluated to 'false', skipping")
+      debug("enable evaluated to 'false', skipping")
     }
+    const client = getOctokit(token)
+    const {payload} = context
+    debug('making request to GitHub')
+    const data = enableAutoMerge(
+      payload as PullRequestEvent,
+      MergeMethod.MERGE,
+      client
+    )
+    debug(`GitHub response:\n${JSON.stringify(data, null, 2)}`)
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) setFailed(error.message)
   }
 }
 
