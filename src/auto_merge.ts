50d98be070d6e7e73470cd98fa8e767f89d9d4be
Ken Howard <ken@kenhoward.dev>
2022-05-14 22:24:56 -0700

    feat: enable auto merge (#11)
    * feat: enable auto merge
    
    * refactor: add await
    
    * refactor: add await
    
    * package
    
    * add permissions
    
    * contents
    
    * user defined merge method
    
    * merge foo
    
    * error handling
    
    * test for invalid merge method
    
    * exit when enable is false
    
    * exit when enable is false

---
+++ src/auto_merge.ts
@@ -0,0 +1,58 @@
+import type {PullRequestEvent} from '@octokit/webhooks-types'
+import type {graphql} from '@octokit/graphql'
+
+interface AutoMergeResponse {
+  clientMutationId: null | string
+  pullRequest: {
+    id: string
+    state: string
+    autoMergeRequest: {
+      enabledAt: string
+      enableBy: {
+        login: string
+      }
+    }
+  }
+}
+
+interface Client {
+  graphql: typeof graphql
+}
+
+export type MergeMethod = 'MERGE' | 'SQUASH' | 'REBASE'
+
+export async function enableAutoMerge(
+  {pull_request: {node_id: pullRequestId}}: PullRequestEvent,
+  mergeMethod: MergeMethod,
+  client: Client
+): Promise<AutoMergeResponse> {
+  return await client.graphql(
+    `
+      mutation(
+        $pullRequestId: ID!,
+        $mergeMethod: PullRequestMergeMethod!
+      ) {
+        enablePullRequestAutoMerge(input: {
+          pullRequestId: $pullRequestId,
+          mergeMethod: $mergeMethod
+        }) {
+          clientMutationId
+          pullRequest {
+            id
+            state
+            autoMergeRequest {
+              enabledAt
+              enabledBy {
+                login
+              }
+            }
+          }
+        }
+      }
+    `,
+    {
+      pullRequestId,
+      mergeMethod
+    }
+  )
+}
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,41 @@
-import * as core from '@actions/core'
+import {MergeMethod, enableAutoMerge} from './auto_merge'
+import {context, getOctokit} from '@actions/github'
+import {debug, getInput, info, setFailed} from '@actions/core'
+import type {PullRequestEvent} from '@octokit/webhooks-types'
 
 export async function run(): Promise<void> {
   try {
-    const enable = Boolean(core.getInput('enable') === 'true')
-    const token = core.getInput('token')
+    const enable = Boolean(getInput('enable') === 'true')
+    const token = getInput('token')
+    const mergeMethod = getInput('merge_method') as MergeMethod
+
+    if (!['MERGE', 'SQUASH', 'REBASE'].includes(mergeMethod)) {
+      throw new Error(
+        `required field 'merge_method' has incorrect value of '${mergeMethod}'` +
+          ` - must be one of MERGE, SQUASH, or REBASE`
+      )
+    }
 
     if (!token) {
       throw new Error("required field 'token' missing or empty")
     }
 
     if (!enable) {
-      core.debug("enable evaluated to 'false', skipping")
+      debug("enable evaluated to 'false', skipping")
+      return
     }
+    const client = getOctokit(token)
+    const {payload} = context
+    debug('making request to GitHub')
+    const data = await enableAutoMerge(
+      payload as PullRequestEvent,
+      mergeMethod,
+      client
+    )
+    debug(`GitHub response:\n${JSON.stringify(data, null, 2)}`)
+    info('auto-merge enabled')
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) setFailed(error.message)
   }
 }
 
